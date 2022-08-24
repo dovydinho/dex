faeef69840ed8ded947cf75c89e87d14300bca87
dovydinho <dovydas.lapinskas@tutanota.com>
2022-08-24 11:28:31 +0300

    Configuration for Rinkeby Test Network Added

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,31 @@
 module.exports = {
-  contracts_build_directory: "./public/contracts",
+  contracts_build_directory: './public/contracts',
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: '127.0.0.1',
       port: 7545,
-      network_id: "*",
+      network_id: '*'
     },
+    rinkeby: {
+      // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: keys.MNEMONIC
+          },
+          providerOrUrl: `https://rinkeby.infura.io/v3/${keys.INFURA_PROJECT_ID}`,
+          addressIndex: 0
+        }),
+      network_id: '4',
+      gas: 5500000, // Gas Limit, How much gas we are willing to spent
+      gasPrice: 20000000000, // how much we are willing to spent for unit of gas
+      confirmations: 2, // number of blocks to wait between deployment
+      timeoutBlocks: 200 // number of blocks before deployment times out
+    }
   },
   compilers: {
     solc: {
-      version: "0.8.10",
-    },
-  },
+      version: '0.8.10'
+    }
+  }
 };
