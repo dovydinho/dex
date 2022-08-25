8c9f8aacf5dfec33de08e51ccda32610e3b92271
dovydinho <dovydas.lapinskas@tutanota.com>
2022-08-25 06:17:38 +0300

    truffle-config.js update

--- truffle-config.js
+++ truffle-config.js
@@ -6,6 +6,22 @@ module.exports = {
       port: 7545,
       network_id: '*'
     },
+    ropsten: {
+      // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: keys.MNEMONIC
+          },
+          providerOrUrl: `https://ropsten.infura.io/v3/${keys.INFURA_PROJECT_ID}`,
+          addressIndex: 0
+        }),
+      network_id: '3',
+      gas: 5500000, // Gas Limit, How much gas we are willing to spent
+      gasPrice: 20000000000, // how much we are willing to spent for unit of gas
+      confirmations: 2, // number of blocks to wait between deployment
+      timeoutBlocks: 200 // number of blocks before deployment times out
+    },
     rinkeby: {
       // must be a thunk, otherwise truffle commands may hang in CI
       provider: () =>
@@ -17,6 +33,22 @@ module.exports = {
           addressIndex: 0
         }),
       network_id: '4',
+      gas: 5500000, // Gas Limit, How much gas we are willing to spent
+      gasPrice: 20000000000, // how much we are willing to spent for unit of gas
+      confirmations: 2, // number of blocks to wait between deployment
+      timeoutBlocks: 200 // number of blocks before deployment times out
+    },
+    kovan: {
+      // must be a thunk, otherwise truffle commands may hang in CI
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: {
+            phrase: keys.MNEMONIC
+          },
+          providerOrUrl: `https://kovan.infura.io/v3/${keys.INFURA_PROJECT_ID}`,
+          addressIndex: 0
+        }),
+      network_id: '42',
       gas: 5500000, // Gas Limit, How much gas we are willing to spent
       gasPrice: 20000000000, // how much we are willing to spent for unit of gas
       confirmations: 2, // number of blocks to wait between deployment
