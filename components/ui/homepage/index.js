df048c09d8c56938724bb3359b24cbf28944f518
dovydinho <dovydas.lapinskas@tutanota.com>
2022-09-21 20:34:02 +0300

    Footer update

--- components/ui/homepage/index.js
+++ components/ui/homepage/index.js
@@ -3,3 +3,4 @@ export { default as NetworkError } from './networkError';
 export { default as LoadingScreenSpinner } from './loadingScreenSpinner';
 export { default as UnsupportedNetwork } from './unsupportedNetwork';
 export { default as NotConnected } from './notConnected';
+export { default as Footer } from './footer';
