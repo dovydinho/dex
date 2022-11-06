9b738297f8f0095607a6a1a24cf541087bc30d26
dovydinho <dovydas.lapinskas@tutanota.com>
2022-11-06 15:36:41 +0200

    Updated button component class

--- components/ui/common/button/index.js
+++ components/ui/common/button/index.js
@@ -2,7 +2,7 @@ export default function Button({ children, className, ...rest }) {
   return (
     <button
       {...rest}
-      className={`border py-4 rounded-3xl font-bold duration-300 ${className}`}
+      className={`border rounded-full font-bold transition py-4 ${className}`}
     >
       {children}
     </button>
