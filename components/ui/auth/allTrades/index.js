cb98a80e04f551495e21b84bd612c0014872bb15
dovydinho <dovydas.lapinskas@tutanota.com>
2022-11-30 22:12:22 +0200

    All trades page cleanup

--- components/ui/auth/allTrades/index.js
+++ components/ui/auth/allTrades/index.js
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react';
-import { LoadingSpinner } from '@components/ui/common';
 import Moment from 'react-moment';
 import InfiniteScroll from 'react-infinite-scroll-component';
+
+import { LoadingSpinner } from '@components/ui/common';
 
 export default function AllTrades({ trades, user, web3 }) {
   const [displayTrades, setDisplayTrades] = useState([]);
@@ -79,84 +80,82 @@ export default function AllTrades({ trades, user, web3 }) {
   };
 
   return (
-    <>
-      <div className="w-full hover:bg-indigo-500/[.25] duration-300 sm:p-6 md:p-12 bg-gray-100/[.05] border rounded-2xl border-indigo-600">
-        <h2 className="text-center text-xl md:text-2xl font-bold mb-8">
-          All Trades
-        </h2>
-        {trades.length > 0 ? (
-          <div className="mb-6">
-            {loading === true ? (
-              <LoadingSpinner additionalClass={'h-80'} />
-            ) : (
-              <>
-                <div className="max-h-[400px] overflow-auto" id="scrollableDiv">
-                  <InfiniteScroll
-                    dataLength={displayTrades.length}
-                    next={addSlice}
-                    hasMore={hasMore}
-                    scrollableTarget="scrollableDiv"
-                    loader={
-                      trades.length > 10 && (
-                        <div className="flex justify-center items-center p-2 bg-white">
-                          <svg
-                            role="status"
-                            className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600"
-                            viewBox="0 0 100 101"
-                            fill="none"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-                              fill="currentColor"
-                            />
-                            <path
-                              d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-                              fill="currentFill"
-                            />
-                          </svg>
-                        </div>
-                      )
-                    }
-                  >
-                    <table className="w-full bg-gray-500">
-                      <thead className="bg-white border-b">
-                        <tr>
-                          <th
-                            scope="col"
-                            className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                          >
-                            Amount, {user.selectedToken.ticker}
-                          </th>
-                          <th
-                            scope="col"
-                            className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                          >
-                            Price, USDC
-                          </th>
-                          <th
-                            scope="col"
-                            className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
-                          >
-                            Activity
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>{displayTrades}</tbody>
-                    </table>
-                  </InfiniteScroll>
-                </div>
-              </>
-            )}
-          </div>
-        ) : (
-          !loading && (
-            <h1 className="italic text-center py-6">
-              There are no trades for this Token.
-            </h1>
-          )
-        )}
-      </div>
-    </>
+    <section className="w-full hover:bg-indigo-500/[.25] duration-300 sm:p-6 md:p-12 bg-gray-100/[.05] border rounded-2xl border-indigo-600">
+      <h2 className="text-center text-xl md:text-2xl font-bold mb-8">
+        All Trades
+      </h2>
+      {trades.length > 0 ? (
+        <div className="mb-6">
+          {loading === true ? (
+            <LoadingSpinner additionalClass={'h-80'} />
+          ) : (
+            <>
+              <div className="max-h-[400px] overflow-auto" id="scrollableDiv">
+                <InfiniteScroll
+                  dataLength={displayTrades.length}
+                  next={addSlice}
+                  hasMore={hasMore}
+                  scrollableTarget="scrollableDiv"
+                  loader={
+                    trades.length > 10 && (
+                      <div className="flex justify-center items-center p-2 bg-white">
+                        <svg
+                          role="status"
+                          className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-purple-600"
+                          viewBox="0 0 100 101"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                        >
+                          <path
+                            d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                            fill="currentColor"
+                          />
+                          <path
+                            d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                            fill="currentFill"
+                          />
+                        </svg>
+                      </div>
+                    )
+                  }
+                >
+                  <table className="w-full bg-gray-500">
+                    <thead className="bg-white border-b">
+                      <tr>
+                        <th
+                          scope="col"
+                          className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                        >
+                          Amount, {user.selectedToken.ticker}
+                        </th>
+                        <th
+                          scope="col"
+                          className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                        >
+                          Price, USDC
+                        </th>
+                        <th
+                          scope="col"
+                          className="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                        >
+                          Activity
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>{displayTrades}</tbody>
+                  </table>
+                </InfiniteScroll>
+              </div>
+            </>
+          )}
+        </div>
+      ) : (
+        !loading && (
+          <h1 className="italic text-center py-6">
+            There are no trades for this Token.
+          </h1>
+        )
+      )}
+    </section>
   );
 }
